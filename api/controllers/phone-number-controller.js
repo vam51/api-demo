491bf070b37b6c11d1d1c0d546061f42ea2fb72d
Nallaparaju <Vamsi.Nallaparaju@team.telstra.com>
2019-06-16 21:25:47 +1000

    added log statements

--- api/controllers/phone-number-controller.js
+++ api/controllers/phone-number-controller.js
@@ -5,10 +5,14 @@ let dbHelper = require('./../helpers/DataBaseHelper');
 module.exports = {getPhoneNumbers, activatePhoneNumber};
 
 function getPhoneNumbers(req, res, next) {
+    console.debug("Inside getPhoneNumbers")
     let customerId = req.swagger.params["customerId"].value;
+    console.info("Get phone numbers for customer id:"+customerId);
     dbHelper.findByCustomerId(customerId).then((result)=>{
+        console.info("Result:"+result);
         res.json(result);    
     },(err)=>{
+        console.error(err);
         res.status(500);
         res.json({
             "errorCode":"503",
@@ -18,9 +22,12 @@ function getPhoneNumbers(req, res, next) {
 }
 
 function activatePhoneNumber(req, res, next) {
+    console.debug("Inside activatePhoneNumber")
     let phoneNumber = req.swagger.params["phonenumber"].value;
+    console.info("Started activation for phone number:"+phoneNumber);
     dbHelper.findByPhoneNumber(phoneNumber).then((result)=>{
         if(result && result.length == 0){
+            console.info("Unable to find the phone number:"+phoneNumber);
             res.status(400);
             res.json({
                 "errorCode":"404",
@@ -31,16 +38,19 @@ function activatePhoneNumber(req, res, next) {
             obj.status = "ACTIVE";
             dbHelper.save(phoneNumber,obj).then((r)=>{
                 if(r == 1){
+                    console.info("Activation success for phone number:"+phoneNumber);
                     res.json({
                         "message":"Number activated sucessfully"
                     });
                 }
             },(err)=>{
+                console.error(err);
                 res.status(500);
                 res.json({"errorCode":"502","errorMessage":"Error while updating the status"});
             });
         }
     },(err)=>{
+        console.error(err);
         res.status(500)
         res.json({"errorCode":"501","errorMessage":"Error while fetching the phone number"});
     });
